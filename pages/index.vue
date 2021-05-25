f1edc1c1133834d1a30df6c8297f798f9f3b432b
Samuel Hufschmid <shufschmid@gmail.com>
2021-05-25 08:19:20 +0200

    removed map

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <HeatMapWithCheckboxes v-bind:locationsOfParties="locationsOfParties" />
+    <!-- <HeatMapWithCheckboxes v-bind:locationsOfParties="locationsOfParties" /> -->
     <h1>Bar-Chart</h1>
     <BarChart />
   </div>
 </template>
 <script>
 import BarChart from '@/components/BarChart.vue'
-import HeatMapWithCheckboxes from '@/components/HeatMapWithCheckboxes'
+// import HeatMapWithCheckboxes from '@/components/HeatMapWithCheckboxes'
 import locationsOfPartiesData from '@/data/locationsOfParties.json'
 export default {
   components: {
-    HeatMapWithCheckboxes,
+    // HeatMapWithCheckboxes,
     BarChart,
   },
   data() {
